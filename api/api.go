d7b3455794c1fbef0124a57d7c45dedc8de40e8c
arbiosu <amea@oregonstate.edu>
2024-06-13 18:51:52 -0400

    api

--- api/api.go
+++ api/api.go
@@ -3,11 +3,65 @@ package api
 import (
 	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/gothello/logic"
 )
 
-func EncodeState(b *logic.Board) []byte {
+/* POST structs */
+type Move struct {
+	Square int `json:"move"`
+}
+
+type OnlinePlayer struct {
+	Name  string `json:"name"`
+	Piece string `json:"piece"`
+}
+
+func GoServer() {
+	http.HandleFunc("/test", test)
+	http.HandleFunc("/init", initOnlinePlayer)
+	log.Println("Go!")
+	http.ListenAndServe(":8080", nil)
+}
+
+func test(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	// CORS
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	p1 := logic.InitializePlayer("Arbi", "O")
+	p2 := logic.InitializePlayer("Tayler", "X")
+	_, bptr := logic.InitializeGame(*p1, *p2)
+	res := encodeState(bptr)
+	w.Write(res)
+}
+
+func initOnlinePlayer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	// CORS
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	switch r.Method {
+	case "POST":
+		var user OnlinePlayer
+		err := json.NewDecoder(r.Body).Decode(&user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+		log.Printf("%s, %s", user.Name, user.Piece)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "ERR: not a POST request")
+	}
+}
+
+func showState(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	// CORS
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+func encodeState(b *logic.Board) []byte {
 	res, err := json.Marshal(b.Board)
 	if err != nil {
 		fmt.Println("ERROR:", err)
--- main.go
+++ main.go
@@ -1,27 +1,10 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
 	"github.com/gothello/api"
-	"github.com/gothello/logic"
 )
 
 func main() {
 	// logic.PlayGame()
-	http.HandleFunc("/test", test)
-	log.Println("Go!")
-	http.ListenAndServe(":8080", nil)
-}
-
-func test(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	// CORS
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	p1 := logic.InitializePlayer("Arbi", "O")
-	p2 := logic.InitializePlayer("Tayler", "X")
-	_, bptr := logic.InitializeGame(*p1, *p2)
-	res := api.EncodeState(bptr)
-	w.Write(res)
+	api.GoServer()
 }
