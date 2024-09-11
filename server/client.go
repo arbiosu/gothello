8232273d70bea8b8f6303df52cb21a5b8ac31a9b
Arberim Ame <amea@oregonstate.edu>
2024-09-11 16:18:16 -0400

    Add playGame(), sendGameState()

--- server/client.go
+++ server/client.go
@@ -2,20 +2,18 @@ package server
 
 import (
 	"log"
+	"strconv"
+	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/gothello/logic"
 )
 
-// Represents the data client and server will send back and forth
-type GameData struct {
-	Name     string      `json:"name"`
-	Opp      string      `jaon:"opp"` // bot opponent name
-	Board    [100]string `json:"board"`
-	Move     string      `json:"move"`
-	Legal    []int       `json:"legal"`
-	O        int         `json:"o"`
-	X        int         `json:"x"`
-	GameOver int         `json:"gameOver"`
+// Represents the messages client and server will send back and forth
+type Message struct {
+	Type    string      `json:"type"`
+	Content interface{} `json:"content"`
 }
 
 type ClientList map[*Client]bool
@@ -23,59 +21,88 @@ type ClientList map[*Client]bool
 type Client struct {
 	conn *websocket.Conn
 	hub  *Hub
-
-	// ch is used to avoid concurrent writes
-	ch chan []byte
+	game *logic.Game
+	sync.RWMutex
 }
 
-func NewClient(c *websocket.Conn, h *Hub) *Client {
+func NewClient(c *websocket.Conn, h *Hub, pl *logic.PlayerList) *Client {
 	return &Client{
 		conn: c,
 		hub:  h,
-		ch:   make(chan []byte),
+		game: logic.NewGame(pl),
 	}
 }
 
-// Read a message from a Client
-func (c *Client) readMessages() {
+func (c *Client) playGame() {
 	defer func() {
 		// Cleanup connection
 		c.hub.removeClient(c)
 	}()
 
+	// Send initial game state
+	c.sendGameState()
+
 	for {
-		msgType, p, err := c.conn.ReadMessage()
+		var msg Message
+		err := c.conn.ReadJSON(&msg)
 		if err != nil {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				log.Printf("Error: could not read message from client! (%v)\n", err)
-				// TODO: handle
-			}
+			log.Printf("Err: %v\n", err)
 			break
 		}
-		log.Println(msgType)
-		log.Println(p)
+		switch msg.Type {
+		case "move":
+			move := msg.Content.(string)
+			conv := convertMove(move)
+			// TODO: mutex
+			c.game.Flip(conv)
+			c.sendGameState()
+			if c.game.State.Turn == "O" {
+				// Wait for a bit before sending next move
+				// TODO: mutex
+				time.Sleep(1000 * time.Millisecond)
+				aiMove := logic.MaxMove(c.game, 3)
+				c.game.Flip(aiMove)
+				c.sendGameState()
+			}
+		case "newGame":
+			// TODO
+			c.game = logic.NewGame(logic.NewPlayerList(logic.NewPlayer("Human", "X"), logic.NewPlayer("Max", "O")))
+			c.sendGameState()
+		}
 	}
 }
 
-// Write a message to a client
-func (c *Client) writeMessages() {
-	defer func() {
-		c.hub.removeClient(c)
-	}()
+func (c *Client) sendGameState() {
+	o, x := c.game.UpdateScore(false)
+	board := c.game.State.Board
+	turn := c.game.State.Turn
+	gameOver := c.game.GameOver()
+	legal := c.game.AvailableMoves()
 
-	for {
-		select {
-		case message, ok := <-c.ch:
-			if !ok {
-				if err := c.conn.WriteMessage(websocket.CloseMessage, nil); err != nil {
-					log.Printf("Error: connection closed! (%v)\n", err)
-				}
-				return
-			}
-			if err := c.conn.WriteMessage(websocket.TextMessage, message); err != nil {
-				log.Printf("Error: failed to send message (%v)\n", err)
-			}
-			log.Println("Message sent")
-		}
+	state := Message{
+		Type: "gameState",
+		Content: map[string]interface{}{
+			"board":    board,
+			"turn":     turn,
+			"legal":    legal,
+			"o":        o,
+			"x":        x,
+			"gameOver": gameOver,
+		},
 	}
+
+	err := c.conn.WriteJSON(state)
+	if err != nil {
+		log.Printf("Err: %v\n", err)
+		// TODO: handle
+	}
+}
+
+// Convert move string to an int TODO: rethink on js side
+func convertMove(move string) int {
+	i, err := strconv.Atoi(move)
+	if err != nil {
+		log.Println("Error converting move to integer: ", err)
+	}
+	return i
 }
